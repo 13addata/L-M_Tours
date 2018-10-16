93c3fdd4fec68d7d75b6016ea6a444475c064797
13addata <jianxing17000@outlook.com>
2018-10-15 22:27:56 -0400

    .

--- js/custom.js
+++ js/custom.js
@@ -1,5 +1,3 @@
-
-// ISOTOPE FILTER
 // jQuery to collapse the navbar on scroll //
 $(window).scroll(function() {
     if ($(".navbar").offset().top > 50) {
@@ -8,15 +6,3 @@ $(window).scroll(function() {
         $(".navbar-fixed-top").removeClass("top-nav-collapse");
     }
 });
-
-
-/* HTML document is loaded. DOM is ready. 
--------------------------------------------*/
-$(function(){
-  // HIDE MOBILE MENU AFTER CLIKING ON A LINK
-  $('.navbar-collapse a').click(function(){
-        $(".navbar-collapse").collapse('hide');
-    });
-
-});
-
