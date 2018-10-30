f8f944996b2871fbeec4b092e214ff6325f3872b
13addata <jianxing17000@outlook.com>
2018-10-29 20:13:33 -0400

    swiper.js

--- js/custom.js
+++ js/custom.js
@@ -7,11 +7,20 @@ $(window).scroll(function() {
     }
 });
 
+
+$(function(){
+  // HIDE MOBILE MENU AFTER CLIKING ON A LINK
+  $('.navbar-collapse a').click(function(){
+        $(".navbar-collapse").collapse('hide');
+    });
+});
+
 var vm = new Vue({
 
     el: "#body",
     data : {
         teamZoom: 0,
+        tourseen: false,
     },
     methods : {
     }
